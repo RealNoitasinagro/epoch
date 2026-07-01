f35fb32e6dc2d8202ad437872ec0cbcdf62a5893
RealEarthling <RealEarthling@users.noreply.github.com>
2026-07-01 14:38:50 +0000

    deploy: e07848337371d45ef0baf4c64170feca35506e19

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "608076977" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "996374759" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
